a086acb469d538fba9b366aa802b55eb502122d4
Naveenkancham123 <67092441+Naveenkancham123@users.noreply.github.com>
2020-06-30 18:35:51 +0530

    initial commit

---
+++ PublisherService/src/main/java/com/pk/publisher/utility/PublisherServiceConstants.java
@@ -0,0 +1,13 @@
+package com.pk.publisher.utility;
+
+/*
+ * PublisherServiceConstants class 
+ */
+public class PublisherServiceConstants {
+  private PublisherServiceConstants() {
+  }
+  public static final String PATTERN="****";
+  public static final String SUCCESSFULLY_PUBLISHED=" Customer object is successfully published ";
+  public static final String  SUCCESS="SUCCESS";
+  public static final String TOPIC = "PublishedTopic";
+}
